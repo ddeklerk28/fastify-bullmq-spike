70d354e62b6063abd18b0bd98b211397cb883dd8
Dirk de Klerk <dirk.deklerk@blackswan.com>
2025-10-31 21:07:22 +0200

    Task: added keyExtractor function to allow user to name jobs

--- batch-download.js
+++ batch-download.js
@@ -1,12 +1,16 @@
 import { getQueueInstance } from "./queue.js";
 
-const bulkAddJobs = async (payloads) => {
+const bulkAddJobs = async (payloads, keyExtractor) => {
     const queue = getQueueInstance();
 
-    const jobs = payloads.map((payload, index) => ({
-        name: `batch-download-job-${index}`,
-        data: payload
-    }));
+    const jobs = payloads.map((payload, index) => {
+        const key = keyExtractor ? keyExtractor(payload, index) : `job-${index}`;
+
+        return ({
+            name: key,
+            data: payload
+         })
+    });
 
     await queue.add(jobs);
 }
