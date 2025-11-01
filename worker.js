4d26280eb4823c7c33515f3aca40ab4f4d2f77fc
Dirk de Klerk <dirk.deklerk@blackswan.com>
2025-11-01 08:06:16 +0200

    Task: added processer parameter to getWorkerInstance

--- worker.js
+++ worker.js
@@ -1,7 +1,6 @@
 import { Worker } from 'bullmq';
+import { QUEUE_NAME } from "./queue.js";
 import IORedis from "ioredis";
-
-const WORKER_NAME = 'reporting-worker';
 
 const connection = new IORedis({
     host: 'localhost',
@@ -11,9 +10,16 @@ const connection = new IORedis({
 
 let worker = null
 
-export const getWorkerInstance = () => {
+const defaultProcessor = (job) => {
+    console.log(`Processing job ${job.id} of type ${job.name}`);
+}
+
+export const getWorkerInstance = (processor) => {
     if (!worker) {
-        worker = new Worker(WORKER_NAME, async (job) => {}, { connection });
+
+        const _processor = processor || defaultProcessor;
+
+        worker = new Worker(QUEUE_NAME, _processor, { connection });
     }
 
     return worker;
