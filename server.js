69fe67ea26272f717796c7ba81967ce08eb2ff26
Dirk de Klerk <dirk.deklerk@blackswan.com>
2025-11-03 10:01:28 +0200

    Refactor: simplified server

--- server.js
+++ server.js
@@ -1,25 +1,46 @@
 import Fastify from 'fastify';
+import { randomUUID } from 'crypto';
 
-import { addJobs, removeJobs } from './messaging/queue/index.js';
-import { initWorker } from "./messaging/worker/index.js";
+import { addJobs } from './messaging/queue/index.js';
+import { initWorker, jobTracker } from "./messaging/worker/index.js";
 import { rootProcessor } from "./messaging/processors/index.js";
 
 // Initialize worker when server starts
 initWorker(rootProcessor);
+
+// We fire the completion event to send message to reporting-graphql
+jobTracker.on('batch:complete', (batchInfo) => {
+    console.log('complete', JSON.stringify(batchInfo, null, 2));
+})
 
 const concepts = [1, 2, 3];
 
 const fastify = Fastify();
 
 fastify.get('/', async (req, res) => {
+    const batchId = randomUUID();
+    jobTracker.startBatch(batchId, concepts.length);
+
     await addJobs(
         concepts,
         'generate_report',
-        (payload, name, index) => `${name}-${payload}-job`
+        (payload, name, index) => `${name}-${payload}-job`,
+        (payload) => ({ payload, batchId })
     );
 
-	return { message: 'Response from \'\/\'' };
+	return { message: `batch with id: '${batch}' created` };
 });
+
+fastify.get('/batch/:id/status', async (req, res) => {
+    const { id } = req.params;
+
+    const batchInfo = jobTracker.getBatchStatus(id);
+
+    return {
+        total: batchInfo.total,
+        completed: batchInfo.completed,
+    }
+})
 
 fastify.listen({ port: 3000 }, (_, address) => {
 	console.log(`Server listening on ${address}`);
